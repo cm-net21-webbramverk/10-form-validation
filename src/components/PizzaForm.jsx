2548de6e6cd7c51aa1963b47156bf371666b6047
David Andersson <lejonmanen@gmail.com>
2022-04-07 11:20:03 +0200

    First field

--- src/components/PizzaForm.jsx
+++ src/components/PizzaForm.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 
+// Tips: börja med en separat designskiss
+// https://codepen.io/david-zocom/pen/xxpWpRq?editors=1100
+
+
 const PizzaForm = () => {
 	const [address, setAddress] = useState('')
 	const [addressTouched, setAddressTouched] = useState(false)
@@ -9,16 +13,22 @@ const PizzaForm = () => {
 	// Address får inte vara en tom sträng
 	// Bör sluta på ett tal - svårt att kolla
 	let addressIsValid = address.trim().length > 0
+	let addressMessage = ''
+	let addressCss = ''
+	if( addressTouched ) {
+		addressCss = addressIsValid ? 'valid' : 'invalid'
+		addressMessage = addressIsValid ? '' : 'Vi behöver din adress för att kunna leverera pizzan.'
+	}
 
 	return (
 		<div className="form">
-			<section>
-				<p> Leveransadress </p>
+			<section className={addressCss}>
+				<label> Leveransadress </label>
 				<input type="text"
 					onChange={event => setAddress(event.target.value)}
 					onBlur={() => setAddressTouched(true)}
 					/>
-					<p> {addressTouched + ''} </p>
+					<p className="message"> {addressMessage} </p>
 			</section>
 		
 		</div>
